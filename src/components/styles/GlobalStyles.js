c262ab9f9abe178012613cdd201b83edc0ae7d5f
Omar Syx <jprzpam@gmail.com>
2021-05-30 10:52:19 +0200

    initial setup

---
+++ src/components/styles/GlobalStyles.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next' ;
+    src: url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap') format('woff2');
+    font-weight: normal;
+    font-stretch: normal;
+  }
+
+  html {
+    --max-width: 1180px;
+    font-size: 10px;
+  }
+
+  body {
+    font-family: 'Poppins', ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    background: var(--color-background);
+    color: var(--color-text);
+  }
+
+  *, *::before, *after {
+    box-sizing: inherit;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--color-secondary);
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  button {
+    font-family:  ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`
+
+export default GlobalStyles
